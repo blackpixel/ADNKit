ac814f820f0b311fbb658498974b1cb0fbdf7f63
Rudy Richter <rudy@blackpixel.com>
2015-06-19 14:40:52 -0400

    i know this isn't right, but upstream isn't right either.

--- ADNKit/ANKClient+ANKUser.m
+++ ADNKit/ANKClient+ANKUser.m
@@ -299,7 +299,7 @@
 
 
 - (ANKJSONRequestOperation *)fetchMutedUsersForUserWithID:(NSString *)userID completion:(ANKClientCompletionBlock)completionHandler {
-	return [self enqueueGETPath:[self endpointPathForUserID:userID endpoint:completionHandler]
+	return [self enqueueGETPath:[self endpointPathForUserID:userID endpoint:(NSString*)completionHandler]
 					 parameters:nil
 						success:[self successHandlerForCollectionOfResourceClass:[ANKUser class] clientHandler:completionHandler]
 						failure:[self failureHandlerForClientHandler:completionHandler]];
